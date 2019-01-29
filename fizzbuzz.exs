898160694f35571a9237c43810835f1ce64d8edb
Ben Vandgrift <ben@vandgrift.com>
2019-01-29 17:38:13 -0500

    adding docs to the scaffold

---
+++ fizzbuzz.exs
@@ -0,0 +1,16 @@
+#!/usr/bin/env elixir
+# we include the above line to let the shell know
+# what interpreter should used in the case that this
+# file is executable
+
+# remember the rules:
+# - if the number is divisible by 3, print 'fizz'
+# - if the number is divisible by 5, print 'buzz'
+# - if the number is divisible by 3 AND 5, print 'fizzbuzz'
+# - otherwise, print the number
+
+# we can solve our exercise with a simple script, runnable
+# from the command line. in fact, this script is executable
+# and can be run in place with `./fizzbuzz.exs`
+
+IO.puts "create a solution here!"
